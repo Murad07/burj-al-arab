eb120a0c7957315e8b437e62e1821d614eb90587
Murad Hossain <sliverboymurad@gmail.com>
2020-09-12 11:18:29 +0600

    context api and private route implement

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,9 +2,20 @@ import React, { useContext } from 'react';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import firebaseConfig from './firebase.config';
+import { UserContext } from './../../App';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const Login = () => {
-  firebase.initializeApp(firebaseConfig);
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
+  const history = useHistory();
+  const location = useLocation();
+
+  let { from } = location.state || { from: { pathname: '/' } };
+
+  if (firebase.apps.length === 0) {
+    firebase.initializeApp(firebaseConfig);
+  }
 
   const handleGoogleSignIn = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
@@ -13,7 +24,13 @@ const Login = () => {
       .auth()
       .signInWithPopup(provider)
       .then((result) => {
-        const user = result.user;
+        const { displayName, email } = result.user;
+        const signInUser = {
+          name: displayName,
+          email: email,
+        };
+        setLoggedInUser(signInUser);
+        history.replace(from);
       })
       .catch(function (error) {
         console.log(error.message);
