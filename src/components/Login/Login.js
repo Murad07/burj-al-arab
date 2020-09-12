78be39ec5b53601e183d169419b8ecf8bed8fcf0
Murad Hossain <sliverboymurad@gmail.com>
2020-09-12 10:11:12 +0600

    firebase config and sign in google implement

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,31 @@
 import React, { useContext } from 'react';
-
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebase.config';
 
 const Login = () => {
-    return (
-        <div>
-            <h1>This is Login</h1>
-        </div>
-    );
+  firebase.initializeApp(firebaseConfig);
+
+  const handleGoogleSignIn = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        const user = result.user;
+      })
+      .catch(function (error) {
+        console.log(error.message);
+      });
+  };
+
+  return (
+    <div>
+      <h1>This is Login</h1>
+      <button onClick={() => handleGoogleSignIn()}>Google Sign In</button>
+    </div>
+  );
 };
 
 export default Login;
