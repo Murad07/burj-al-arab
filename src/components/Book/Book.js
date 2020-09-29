3b537bda638f9cdccb71e0314f28393b40ffe6d4
Murad Hossain <sliverboymurad@gmail.com>
2020-09-29 09:34:41 +0600

    add material ui date picker

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,14 +1,81 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { UserContext } from '../../App';
+
+import 'date-fns';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
 
 const Book = () => {
-    const {bedType} = useParams();
-    return (
-        <div style={{textAlign: 'center'}}>
-            <h1>Let's book a {bedType} Room.</h1>
-            <p>Want a <Link to="/home">different room?</Link> </p>
-        </div>
-    );
+  const { bedType } = useParams();
+  const [loggeddInUser, setLoggedInUser] = useContext(UserContext);
+
+  const [selectedDate, setSelectedDate] = useState({
+    checkIn: new Date(),
+    chekOut: new Date(),
+  });
+
+  const handleCheckInDate = (date) => {
+    const newDates = { ...selectedDate };
+    newDates.checkIn = date;
+    setSelectedDate(newDates);
+  };
+
+  const handleCheckOutDate = (date) => {
+    const newDates = { ...selectedDate };
+    newDates.checkOut = date;
+    setSelectedDate(newDates);
+  };
+
+  const handleBooking = () => {};
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Hello, {loggeddInUser.name}! Let's book a {bedType} Room.
+      </h1>
+      <p>
+        Want a <Link to='/home'>different room?</Link>{' '}
+      </p>
+
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Grid container justify='space-around'>
+          <KeyboardDatePicker
+            disableToolbar
+            variant='inline'
+            format='dd/MM/yyyy'
+            margin='normal'
+            id='date-picker-inline'
+            label='Check In Date'
+            value={selectedDate.checkIn}
+            onChange={handleCheckInDate}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+          <KeyboardDatePicker
+            margin='normal'
+            id='date-picker-dialog'
+            label='Check Out Date'
+            format='dd/MM/yyyy'
+            value={selectedDate.chekOut}
+            onChange={handleCheckOutDate}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+        </Grid>
+      </MuiPickersUtilsProvider>
+      <Button onClick='handleBooking' variant='contained' color='primary'>
+        Book Now
+      </Button>
+    </div>
+  );
 };
 
 export default Book;
